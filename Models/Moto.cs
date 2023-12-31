bc9d93f9bc2657fcfa06f4a31ecfe5926363b89b
Bianca Bringel <bbringel@outlook.com>
2023-12-30 21:33:24 -0500

    Add files via upload

---
+++ Models/Moto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMoto.Models;
+
+public class Moto
+{
+    public int Id { get; set; }
+
+    [StringLength(30, MinimumLength = 3)]
+    [Required]
+    public string? Name { get; set; }
+
+    [StringLength(10, MinimumLength = 3)]
+    [Required]
+    public string? Circuit { get; set; }
+
+    [StringLength(10, MinimumLength = 3)]
+    [Required]
+    public string? Team { get; set; }
+
+    [Display(Name = "World Rank")]
+    public decimal WorldRank { get; set; }
+
+    [Display(Name = "Driver Number")]
+    public string? DriverNumber { get; set; }
+}
+
---
+++ Models/SeedData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMoto.Data;
+using System;
+using System.Linq;
+
+namespace MvcMoto.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMotoContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMotoContext>>()))
+        {
+ 
+            if (context.Moto.Any())
+            {
+                return;   // Database has been seeded
+            }
+            context.Moto.AddRange(
+                new Moto
+                {
+                    Name = "Max Verstappen",
+                    Circuit = "F1",
+                    Team = "Red Bull Racing",
+                    WorldRank = 1,
+                    DriverNumber = "11"
+                },
+
+                new Moto
+                {
+                    Name = "Daniel Ricciardo",
+                    Circuit = "F1",
+                    Team = "Alpha Tauri",
+                    WorldRank = 5,
+                    DriverNumber = "48"
+                },
+
+                new Moto
+                {
+                    Name = "George Russel",
+                    Circuit = "F1",
+                    Team = "AMG Mercedes",
+                    WorldRank = 4,
+                    DriverNumber = "25"
+                                    },
+
+                new Moto
+                {
+                    Name = "Lando Norris",
+                    Circuit = "F1",
+                    Team = "McLaren",
+                    WorldRank = 8,
+                    DriverNumber = "5"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
+
