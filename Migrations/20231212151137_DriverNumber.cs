85def148de58606642082e7ca2429bf2e793a7e8
Bianca Bringel <bbringel@outlook.com>
2023-12-30 21:33:36 -0500

    Add files via upload

---
+++ Migrations/20231212151137_DriverNumber.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMoto.Migrations
+{
+    /// <inheritdoc />
+    public partial class DriverNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DriverNumber",
+                table: "Moto",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DriverNumber",
+                table: "Moto");
+        }
+    }
+}
